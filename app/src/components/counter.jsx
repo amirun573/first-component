0c18cd6be53b774e9ba328b4dc100d54df6e40ce
amirun573 <amirunaqmar@gmail.com>
2021-01-24 01:31:11 +0800

    Learn Button

--- app/src/components/counter.jsx
+++ app/src/components/counter.jsx
@@ -3,7 +3,13 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     render() {
-        return <h1>Hello World</h1>;
+        return (
+
+            <div>
+                <h1>Hello World</h1>
+                <button>Increament</button>
+            </div>
+        );
     }
     
 }
