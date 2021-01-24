4ba760efddcf577e9c4afcd5d469dbf21979b610
amirun573 <amirunaqmar@gmail.com>
2021-01-24 15:31:15 +0800

    Use JSX to return  element

--- app/src/components/counter.jsx
+++ app/src/components/counter.jsx
@@ -20,7 +20,7 @@ class Counter extends Component {
 
     formatCount(){
         const {count} = this.state; 
-        return count === 0 ? 'Zero' : count;
+        return count === 0 ? <h1>Zero</h1> : count;
     }
     
 }
