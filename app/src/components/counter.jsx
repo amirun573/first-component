49b1651c32fdbd38b89adabe7b18cd2ec6fd61de
amirun573 <amirunaqmar@gmail.com>
2021-01-24 15:30:18 +0800

    Create formatCount method to check state.count is 0 or not

--- app/src/components/counter.jsx
+++ app/src/components/counter.jsx
@@ -12,12 +12,19 @@ class Counter extends Component {
         return (
 
             <React.Fragment>
-                <span>{this.state.count}</span>
+                <span>{this.formatCount()}</span>
                 <button>Increment</button>
             </React.Fragment>
         );
     }
+
+    formatCount(){
+        const {count} = this.state; 
+        return count === 0 ? 'Zero' : count;
+    }
     
 }
+
+
  
 export default Counter;
