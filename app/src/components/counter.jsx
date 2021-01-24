3625c60844a3f32538c28927623597fcd1ebafa2
amirun573 <amirunaqmar@gmail.com>
2021-01-24 15:34:59 +0800

    Use button class Bootstrap

--- app/src/components/counter.jsx
+++ app/src/components/counter.jsx
@@ -13,7 +13,7 @@ class Counter extends Component {
 
             <React.Fragment>
                 <span className="badge badge-primary m-2">{this.formatCount()}</span>
-                <button>Increment</button>
+                <button className="btn btn-secondary btn-sm">Increment</button>
             </React.Fragment>
         );
     }
