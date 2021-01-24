ea86fa1c2d9a3f43ea387304c7ae876a13515ee3
amirun573 <amirunaqmar@gmail.com>
2021-01-24 16:02:08 +0800

    Create list from array

--- app/src/components/counter.jsx
+++ app/src/components/counter.jsx
@@ -6,6 +6,7 @@ class Counter extends Component {
     state = {
 
         count:0,
+        tags: ['tag1', 'tag2', 'tag3']
         
     };
 
@@ -24,6 +25,9 @@ class Counter extends Component {
             <React.Fragment>
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button className="btn btn-secondary btn-sm">Increment</button>
+                <ul>
+                    { this.state.tags.map(tag =><li></li>)}
+                </ul>
             </React.Fragment>
         );
     }
