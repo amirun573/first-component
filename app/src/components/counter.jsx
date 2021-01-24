b3942b6d9ebddc4c78cb8e1fc945defa42a6b76e
amirun573 <amirunaqmar@gmail.com>
2021-01-24 15:34:08 +0800

    Use Medium size

--- app/src/components/counter.jsx
+++ app/src/components/counter.jsx
@@ -12,7 +12,7 @@ class Counter extends Component {
         return (
 
             <React.Fragment>
-                <span className="badge badge-primary">{this.formatCount()}</span>
+                <span className="badge badge-primary m-2">{this.formatCount()}</span>
                 <button>Increment</button>
             </React.Fragment>
         );
